9a7eb6cb32d879fbe466811e7dcd5a6847ae663f
Wilson Junior <wilsonraj@unipam.edu.br>
2021-12-14 23:00:55 -0300

    Fazendo os primmeiros testes de movimentação

--- projetoXadrez/Tela.cs
+++ projetoXadrez/Tela.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace projetoXadrez
 {
@@ -42,6 +43,14 @@ namespace projetoXadrez
                 Console.Write(peca);
                 Console.ForegroundColor = aux;
             }
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
     }
 }
