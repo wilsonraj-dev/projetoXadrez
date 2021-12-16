c0b20094b554df4d3e125eebb210f9f2dd2729b3
Wilson Junior <wilsonraj@unipam.edu.br>
2021-12-16 20:55:06 -0300

    Correção: renomeando nome do método para movimentoPossivel

--- projetoXadrez/Tabuleiro/Peca.cs
+++ projetoXadrez/Tabuleiro/Peca.cs
@@ -39,7 +39,7 @@
             return false;
         }
 
-        public bool podeMoverPara(Posicao pos)
+        public bool movimentoPossivel(Posicao pos)
         {
             return movimentosPossiveis()[pos.linha, pos.coluna];
         }
